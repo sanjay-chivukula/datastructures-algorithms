63c0fd2d94c66f753bd82ddd7679a2e698320468
Sanjay Chivukula <sanjay.chivukula@gmail.com>
2020-08-12 20:05:07 -0400

    Update trie-basic.h: created basic trie class structure.

--- trie-cpp/trie-basic.h
+++ trie-cpp/trie-basic.h
@@ -5,4 +5,36 @@
 #ifndef TRIE_CPP_TRIE_BASIC_H
 #define TRIE_CPP_TRIE_BASIC_H
 
+#include <iostream>
+
+
+static const int CHARACTER_COUNT = 26;
+
+
+struct TrieNode {
+    int value;
+    TrieNode *children[CHARACTER_COUNT]{nullptr};
+};
+
+
+class Trie {
+
+public:
+    Trie();
+
+    ~Trie();
+
+    void insertItem(const std::string word, int value);
+
+    bool deleteItem(const std::string word);                // returns true even if item doesn't exist.
+
+    bool hasKey(const std::string word) const;
+
+    int getValue(const std::string word) const;             // throws error if word/key doesn't exist.
+
+private:
+    TrieNode *rootNode{nullptr};
+
+};
+
 #endif //TRIE_CPP_TRIE_BASIC_H
