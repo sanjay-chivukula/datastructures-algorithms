43cf47d4b94b7c7f0a494cd16248e51656884e69
Sanjay Chivukula <sanjay.chivukula@gmail.com>
2020-08-27 20:08:42 -0400

    Update graph-cpp: added code for graph-adjacency-list header.

--- graph-cpp/graph-adjacency-list.h
+++ graph-cpp/graph-adjacency-list.h
@@ -5,4 +5,31 @@
 #ifndef GRAPH_CPP_GRAPH_ADJACENCY_LIST_H
 #define GRAPH_CPP_GRAPH_ADJACENCY_LIST_H
 
+#include <iostream>
+#include <vector>
+
+class GraphAdjacecnyList {
+    GraphAdjacecnyList() = default;
+
+    ~GraphAdjacecnyList() = default;
+
+    void addVertex();
+
+    void updateEdgeWeight(const int &row, const int &col, const int &weight);
+
+    int vertexCount();
+
+    int operator()(const int &row, const int &col) const;
+
+    [[nodiscard]] int getEdgeWeight(const int &row, const int &col) const;
+
+    [[nodiscard]] bool hasEdge(const int &row, const int &col) const;
+
+    void printGraph();
+
+private:
+    std::vector<std::vector<std::pair<int, int>>> graph;
+};
+
+
 #endif //GRAPH_CPP_GRAPH_ADJACENCY_LIST_H
