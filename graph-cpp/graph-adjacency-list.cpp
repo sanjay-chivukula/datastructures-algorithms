fe0864a2df00eb99af42018360dcbc0a60c14259
Sanjay Chivukula <sanjay.chivukula@gmail.com>
2020-08-29 17:04:49 -0400

    Update graph-cpp project: update adjacency list cpp source, implemented updateEdgeWeight().

--- graph-cpp/graph-adjacency-list.cpp
+++ graph-cpp/graph-adjacency-list.cpp
@@ -2,3 +2,25 @@
 // Created by sanjay on 8/27/20.
 //
 
+#include "graph-adjacency-list.h"
+
+void GraphAdjacecnyList::addVertex() {
+    graph.emplace_back(std::vector<std::pair<int, int>>());
+}
+
+void GraphAdjacecnyList::updateEdgeWeight(const int &src, const int &dst, const int &weight) {
+    if (src >= graph.size()){
+        return;
+    }
+
+    for (auto &edgePair: graph[src]) {
+        if (edgePair.first == dst) {
+            edgePair.second = weight;
+            return;
+        }
+    }
+
+    graph[src].emplace_back(std::pair<int, int>(dst, weight));
+}
+
+
