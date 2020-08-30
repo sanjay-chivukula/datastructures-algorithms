c69f7522b3cc1114d633f2c91d89fe811bfcc26e
Sanjay Chivukula <sanjay.chivukula@gmail.com>
2020-08-29 20:38:46 -0400

    Update graph-cpp project: update adjacency list cpp source, implemented other functions.

--- graph-cpp/graph-adjacency-list.cpp
+++ graph-cpp/graph-adjacency-list.cpp
@@ -1,8 +1,11 @@
 //
 // Created by sanjay on 8/27/20.
 //
+// This implementation breaks if a node of the graph is deleted, or makes it more complex to delete a node.
 
 #include "graph-adjacency-list.h"
+
+
 
 void GraphAdjacecnyList::addVertex() {
     graph.emplace_back(std::vector<std::pair<int, int>>());
@@ -40,3 +43,30 @@ int GraphAdjacecnyList::operator()(const int &src, const int &dst) const {
     throw std::out_of_range("This edge does not exist");
 }
 
+int GraphAdjacecnyList::getEdgeWeight(const int &src, const int &dst) const {
+    return this->operator()(src, dst);
+}
+
+
+bool GraphAdjacecnyList::hasEdge(const int &src, const int &dst) const {
+    if (src >= graph.size()) {
+        return false;
+    }
+
+    for (auto &edgePair: graph[src]) {
+        if (edgePair.first == dst) {
+            return true;
+        }
+    }
+    return false;
+}
+
+void GraphAdjacecnyList::printGraph() {
+    for (int srcIdx = 0; srcIdx < graph.size(); ++srcIdx) {
+        std::cout << srcIdx << " -> [ ";
+        for (auto &pairEdge: graph[srcIdx]) {
+            std::cout << "(" << pairEdge.first << ", " << pairEdge.second << ") ";
+        }
+        std::cout << "]" << std::endl;
+    }
+}
